65eb84ac8acedccbdea4e240123694069c42ff2d
Pranav <pranavjain4325@gmail.com>
2026-08-29 16:19:35 +0530

    Updated socket.jsx

--- src/Providers/Socket.jsx
+++ src/Providers/Socket.jsx
@@ -20,7 +20,7 @@ export const SocketProvider = (props) => {
             console.log('🔐 Token (first 20 chars):', token.substring(0, 20) + '...');
         }
 
-        const socketInstance = io('http://3.110.101.93:3000', {
+        const socketInstance = io('https://trickish-urijah-pachydermatously.ngrok-free.dev', {
             auth: {
                 token: token
             },
